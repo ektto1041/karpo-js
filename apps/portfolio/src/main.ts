bce8bbbd2f7dd8582679db40dd1c27d3c5f9be30
ektto1041 <dhkdwk1041@gmail.com>
2025-05-03 14:02:20 +0900

    feat: 기술 스택 그래프 추가

--- apps/portfolio/src/main.ts
+++ apps/portfolio/src/main.ts
@@ -1,3 +1,33 @@
-const a: string = "asf";
+import type { ChartConfiguration, ChartData } from "chart.js";
 
-console.log(a, "asfd", "asdf");
+const data: ChartData<"doughnut"> = {
+  labels: ["javascript", "typescript", "React.js"],
+  datasets: [
+    {
+      data: [100, 100, 100],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      borderRadius: 8,
+    },
+  ],
+};
+
+const config: ChartConfiguration<"doughnut"> = {
+  type: "doughnut",
+  data: data,
+  options: {
+    rotation: -90,
+    circumference: 180,
+    aspectRatio: 2,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+    },
+  },
+};
+
+new Chart(document.getElementById("graph") as HTMLCanvasElement, config);
