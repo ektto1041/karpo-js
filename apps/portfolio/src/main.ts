e73d20e729d07af2f192db59a210dd773bf8b37a
ektto1041 <dhkdwk1041@gmail.com>
2025-07-28 12:37:59 +0900

    feat: Modal 구현

--- apps/portfolio/src/main.ts
+++ apps/portfolio/src/main.ts
@@ -30,3 +30,46 @@ const config: ChartConfiguration<"doughnut"> = {
 console.log(stackStatistics);
 
 new Chart(document.getElementById("graph") as HTMLCanvasElement, config);
+
+// dev-card onClick
+const html = document.querySelector("html") as HTMLHtmlElement;
+
+const devCards: HTMLLIElement[] = Array.from(
+  document.getElementsByClassName("dev-card")
+) as HTMLLIElement[];
+
+devCards.forEach((devCard) => {
+  devCard.addEventListener("click", () => {
+    const modalBackground = devCard.querySelector(
+      ".modal-background"
+    ) as HTMLDivElement | null;
+
+    if (modalBackground) modalBackground.style.display = "flex";
+
+    html.style.overflow = "hidden";
+  });
+});
+
+function onModalBackgroundClick(modalBackground: HTMLDivElement) {}
+
+const modalBackgrounds = Array.from(
+  document.querySelectorAll(".modal-background")
+) as HTMLDivElement[];
+
+modalBackgrounds.forEach((modalBackground) => {
+  modalBackground.addEventListener("click", (e) => {
+    e.stopPropagation();
+    modalBackground.style.display = "none";
+    html.style.overflow = "auto";
+  });
+});
+
+const modalBoxes = Array.from(
+  document.querySelectorAll(".modal-box")
+) as HTMLDivElement[];
+
+modalBoxes.forEach((modalBox) => {
+  modalBox.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+});
