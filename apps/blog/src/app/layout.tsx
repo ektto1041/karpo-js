c073e78da21a664ef054d12ea39ec670914b3395
ektto1041 <dhkdwk1041@gmail.com>
2025-08-30 20:57:33 +0900

    feat: blog app 추가

---
+++ apps/blog/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "@karponents-ui/karpotic/dist/style.css";
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Karpo-js",
+  description: "Karpo's Blog",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
